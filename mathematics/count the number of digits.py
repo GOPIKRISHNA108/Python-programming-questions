e78a2f055873b7aa3a32c79fb8d0b1eed6d1542f
GOPIKRISHNA108 <139993196+GOPIKRISHNA108@users.noreply.github.com>
2025-06-24 13:34:35 +0530

    Update count the number of digits.py

--- mathematics/count the number of digits.py
+++ mathematics/count the number of digits.py
@@ -1,1 +1,19 @@
+def count_digits(n):
+    count = 0
+    if n == 0:
+        return 1
+    if n < 0:
+        n = -n  # Make it positive
+    while n > 0:
+        n = n // 10
+        count += 1
+    return count
+
+# Input from user
+try:
+    num = int(input("Enter an integer: "))
+    result = count_digits(num)
+    print("Number of digits:", result)
+except:
+    print("Invalid input! Please enter an integer.")
 
